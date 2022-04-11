4526ec52fa94bb31f3dc1161ed491a30062b1178
getGit789 <101603681+getGit789@users.noreply.github.com>
2022-04-11 05:11:17 +0200

    Delete World Cup Simulator directory

--- World Cup Simulator/index.js
+++
@@ -1,9 +0,0 @@
-import simulateGroups from './tournamentLogic';
-
-const main = () => {
-    const output = simulateGroups();
-
-    document.getElementById("output").innerHTML = output;
-}
-
-main();
